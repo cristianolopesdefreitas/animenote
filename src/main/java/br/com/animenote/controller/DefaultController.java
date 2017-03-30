8b66b66843a0a26efb51c56f2f42ef181cd1c791
Cristiano Lopes de Freitas <cristiano.freitas@tqi.com.br>
2017-03-30 18:05:46 -0300

    Configuração de includes

--- src/main/java/br/com/animenote/controller/DefaultController.java
+++ src/main/java/br/com/animenote/controller/DefaultController.java
@@ -1,13 +1,8 @@
 package br.com.animenote.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DefaultController {
 	
-	@GetMapping("/error")
-	public String pageNotFound() {
-		return "404";
-	}
 }
---
+++ src/main/resources/static/js/formValidateSettings.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+
+    function startSettings() {
+        $.validator.setDefaults({
+            highlight: function( element ) {
+                $( element ).closest( '.form-group' ).addClass( 'has-error' );
+            },
+            unhighlight: function( element ) {
+                $( element ).closest( '.form-group' ).removeClass( 'has-error' );
+            },
+            errorElement: 'span',
+            errorClass: 'help-block',
+            errorPlacement: function( error, element ) {
+                if ( element.parent( '.input-group' ).length ) {
+                    error.insertAfter( element.parent() );
+                } else if ( element.closest( '.radio' ).length ) {
+                    error.insertAfter( element.closest( '.form-group' ).find( '.radio' ).last() );
+                } else {
+                    error.insertAfter( element );
+                }
+            }
+        });
+    }
+
+    startSettings();
+
+}());
